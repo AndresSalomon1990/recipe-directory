165058317b80a55a5ef35757818867cef6c625b8
AndresSalomon1990 <a.salomon.1990@gmail.com>
2022-02-28 13:54:15 -0300

    Add multiple ingredients functionality to the Create form

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,14 +1,30 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './Create.css';
 
 export default function Create() {
   const [title, setTitle] = useState('');
   const [method, setMethod] = useState('');
   const [cookingTime, setCookingTime] = useState('');
+  const [newIngredient, setNewIngredient] = useState(''); // track 1 ingredient
+  const [ingredients, setIngredients] = useState([]); // track all ingredients
+  const ingredientInput = useRef(); // to focus the input after adding an ingredient
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(title, method, cookingTime);
+    console.log(title, method, cookingTime, ingredients);
+  };
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    const ing = newIngredient.trim();
+
+    // prevent adding the same ingredient
+    if (ing && !ingredients.includes(ing)) {
+      setIngredients((prevIngredients) => [...prevIngredients, ing]);
+    }
+
+    setNewIngredient('');
+    ingredientInput.current.focus();
   };
 
   return (
@@ -25,7 +41,26 @@ export default function Create() {
           />
         </label>
 
-        {/* ingredients go here */}
+        <label>
+          <span>Recipe ingredients:</span>
+          <div className='ingredients'>
+            <input
+              type='text'
+              onChange={(e) => setNewIngredient(e.target.value)}
+              value={newIngredient}
+              ref={ingredientInput}
+            />
+            <button className='btn' onClick={handleAdd}>
+              Add
+            </button>
+          </div>
+        </label>
+        <p>
+          Current ingredients:{' '}
+          {ingredients.map((i) => (
+            <em key={i}>{i}, </em>
+          ))}
+        </p>
 
         <label>
           <span>Recipe method:</span>
@@ -44,7 +79,7 @@ export default function Create() {
             required
           />
         </label>
-        <button>Submit</button>
+        <button className='btn'>Submit</button>
       </form>
     </div>
   );
