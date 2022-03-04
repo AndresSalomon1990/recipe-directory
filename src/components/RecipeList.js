7475a1ae88abb5a80daecd7c4f66cdc00b650cb0
AndresSalomon1990 <a.salomon.1990@gmail.com>
2022-03-04 00:35:04 -0300

    Add real-time collection data in Home component

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,9 +1,15 @@
 import { Link } from 'react-router-dom';
 import { useTheme } from '../hooks/useTheme';
+import Thrashcan from '../assets/thrashcan.svg';
+import { projectFirestore } from '../firebase/config';
 import './RecipeList.css';
 
 export default function RecipeList({ recipes }) {
   const { mode } = useTheme();
+
+  const handleClick = (id) => {
+    projectFirestore.collection('recipes').doc(id).delete()
+  };
 
   if (recipes.length === 0) {
     // if there aren't recipes matched
@@ -18,6 +24,12 @@ export default function RecipeList({ recipes }) {
           <p>{recipe.cookingTime} to make.</p>
           <div>{recipe.method.substring(0, 100)}...</div>
           <Link to={`/recipes/${recipe.id}`}>Cook this</Link>
+          <img
+            src={Thrashcan}
+            alt='thrash can'
+            className='delete'
+            onClick={() => handleClick(recipe.id)}
+          />
         </div>
       ))}
     </div>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -11,10 +11,8 @@ export default function Home() {
   useEffect(() => {
     setIsPending(true);
 
-    projectFirestore
-      .collection('recipes')
-      .get()
-      .then((snapshot) => {
+    const unsub = projectFirestore.collection('recipes').onSnapshot(
+      (snapshot) => {
         if (snapshot.empty) {
           // no docs in the db
           setError('No recipes to load');
@@ -27,11 +25,14 @@ export default function Home() {
           setData(results);
           setIsPending(false);
         }
-      })
-      .catch((e) => {
-        setError(e.message);
+      },
+      (err) => {
+        setError(err.message);
         setIsPending(false);
-      });
+      }
+    );
+
+    return () => unsub();
   }, []);
 
   return (
